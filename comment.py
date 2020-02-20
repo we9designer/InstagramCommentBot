1cb9305d7e55e4cbb27305c0a909bf9bd15d7370
kamransadixov <kamransadixov@mail.ru>
2020-02-20 20:04:50 +0400

    make it better

--- comment.py
+++ comment.py
@@ -3,10 +3,6 @@ from selenium.webdriver.common.keys import Keys
 import time
 import sys
 
-website = "https://www.instagram.com/"
-username = "YOUR USERNAME"
-password = "YOUR PASSWORD"
-message = "YOUR MESSAGE"
 
 def print_same_line(text):
 	sys.stdout.write('\r')
@@ -14,58 +10,65 @@ def print_same_line(text):
 	sys.stdout.write(text)
 	sys.stdout.flush()
 
+while True:
+	website = "https://www.instagram.com/"
+	username = "mybotforyou"
+	password = "kam125486"
+	message = "YOUR MESSAGE"
 
-class InstagramBot:
 
-	def __init__(self, username, password):
-		self.username = username
-		self.password = password
-		self.browser = webdriver.Firefox()
-		self.browser.set_window_size(1100, 900)
 
-	def closeBrowser(self):
-		self.browser.close()
+	class InstagramBot:
 
-	def login(self):
-		browser = self.browser
-		start_time = time.monotonic()
-		browser.get(website)
-		time.sleep(1)
+		def __init__(self, username, password):
+			self.username = username
+			self.password = password
+			self.browser = webdriver.Firefox()
+			self.browser.set_window_size(1100, 900)
 
-		#Login button
-		login_button = browser.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
-		login_button.click()
-		time.sleep(1)
+		def closeBrowser(self):
+			self.browser.close()
 
-		#Username input
-		user_name_elem = browser.find_element_by_css_selector('div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)')
-		user_name_elem.clear()
-		user_name_elem.send_keys(self.username)
+		def login(self):
+			browser = self.browser
+			start_time = time.monotonic()
+			browser.get(website)
+			time.sleep(1)
 
-		#Password input
-		passworword_elem = browser.find_element_by_css_selector('div.-MzZI:nth-child(3) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)')
-		passworword_elem.clear()
-		passworword_elem.send_keys(self.password)
-		passworword_elem.send_keys(Keys.RETURN)
-		time.sleep(3)
+			#Login button
+			login_button = browser.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
+			login_button.click()
+			time.sleep(1)
 
-		#Copy url after https://instagram.com/ into quotations marks 
-		browser.get(website + "")
-		time.sleep(1)
-		
-		#Comment
-		for x in range(2):
-			try:
-				comment_box_elem = browser.find_element_by_css_selector(".Ypffh")
-				comment_box_elem.clear()
-				comment_box_elem.send_keys(message)
-				comment_box_elem.send_keys(Keys.ENTER)
-				time.sleep(1)
-			except:
-				pass
-		browser.close()
+			#Username input
+			user_name_elem = browser.find_element_by_css_selector('div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)')
+			user_name_elem.clear()
+			user_name_elem.send_keys(self.username)
 
-		print("Exit time: 			",(f'{time.monotonic()-start_time}'))
+			#Password input
+			passworword_elem = browser.find_element_by_css_selector('div.-MzZI:nth-child(3) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)')
+			passworword_elem.clear()
+			passworword_elem.send_keys(self.password)
+			passworword_elem.send_keys(Keys.RETURN)
+			time.sleep(3)
 
-andreyIG = InstagramBot(username, password)
-andreyIG.login()
+			#Copy url after https://instagram.com/ into quotations marks 
+			browser.get(website + "p/B8ZbouiBDFr/")
+			time.sleep(1)
+			
+			#Comment
+			for x in range(2):
+				try:
+					comment_box_elem = browser.find_element_by_css_selector(".Ypffh")
+					comment_box_elem.clear()
+					comment_box_elem.send_keys(message)
+					comment_box_elem.send_keys(Keys.ENTER)
+					time.sleep(2)
+				except:
+					pass
+			browser.close()
+
+			print("Exit time: 			",(f'{time.monotonic()-start_time}'))
+
+	andreyIG = InstagramBot(username, password)
+	andreyIG.login()
